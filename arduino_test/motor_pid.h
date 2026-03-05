f638a36f95acbfa6258b2449d1836cbde699d5a0
Mr-Moody <118393511+Mr-Moody@users.noreply.github.com>
2026-03-05 15:31:53 +0000

    FEAT: MOTOR PIDS TUNED

--- arduino_test/motor_pid.h
+++ arduino_test/motor_pid.h
@@ -3,10 +3,10 @@
 
 #include <Arduino.h>
 
-#define MOTOR_KP 400.0
+#define MOTOR_KP 1000.0
 #define MOTOR_KI 1000.0
-#define MOTOR_KD 0.0 // 20.0
-#define INTEGRAL_LIMIT 200.0
+#define MOTOR_KD 10.0 // 20.0
+#define INTEGRAL_LIMIT 400.0
 
 int16_t compute_pid_front_left(float desired_speed, float actual_speed, float dt);
 int16_t compute_pid_front_right(float desired_speed, float actual_speed, float dt);
